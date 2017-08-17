f35d9635d50f01e9ddeb1b91f9e2827056773487
ziqiangxu <1401357890@qq.com>
2017-08-17 20:08:22 +0800

    fast-commit

--- install/install.sh
+++ install/install.sh
@@ -7,8 +7,8 @@ make
 sudo mkdir /usr/share/git-blog-editor
 mkdir ~/.config/git-blog-editor
 
-sudo cp -rf ./* /usr/share/git-blog-editor/
+sudo cp -rf mymainwindow /usr/share/git-blog-editor/
 
 cd ../install
-cp -f ./git-blog-editor.ico ~/.config/git-blog-editor/
+sudo cp -f ./editor.jpg /usr/share/icons/hicolor/scalable/apps
 sudo cp -f ./git-blog-editor.desktop /usr/share/applications
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -2,4 +2,5 @@
 rm -rf ../build
 sudo rm -rf /usr/share/git-blog-editor/
 sudo rm -rf /usr/share/applications/git-blog-editor.desktop
+sudo rm -f /usr/share/icons/hicolor/scalable/apps/editor.jpg
 rm -rf ~/.config/git-blog-editor
